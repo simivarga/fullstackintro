630d2eb0f81091eb8a68e1d779d31b3321db27e8
Simi <simike.varga@gmail.com>
2024-02-11 21:15:43 +0100

    ddddd

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,38 @@
-import styles from "./page.module.css";
+import Link from 'next/link';
+import styles from './page.module.css'
 import prisma from '@/lib/prisma'
 
 async function getPosts(){
   const posts = await prisma.post.findMany({
-    where: {published: true}, 
+    where: {published: true},
     include: {
       author: {
         select: {name: true}
       }
     }
   })
+  return posts;
 }
+
 export default async function Home() {
   const posts = await getPosts();
-  console.log({posts})
   return (
-    <main className = {styles.main}>
+    <main className={styles.main}>
+      <Link href={'/add-post'}>Add Post</Link>
       <h1>Feed</h1>
+      {
+        posts.map((post) => {
+          return (
+            <post
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            content={post.content}
+            authorName={post.author.name}
+            />
+          )
+        })
+      }
     </main>
   )
 }
